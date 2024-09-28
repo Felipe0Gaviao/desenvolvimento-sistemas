e86927775f55b347f0fe209a8fc633eea1764503
Felipe Gavião <batatasteve304@gmail.com>
2024-09-28 14:03:25 +0000

    empilhar

--- logica-programacao/pilhas/exercicios/1.py
+++ logica-programacao/pilhas/exercicios/1.py
@@ -3,3 +3,24 @@ import os
 os.system("clear || cls")
 
 pilha = []
+
+
+def empilhar():
+    print("""Insira o nome do item que vai ser adicionado a pilha
+Insira (S) para sair deste comando
+---------------------------------------------""")
+    item = input("")
+
+
+    while True:
+        if item == "":
+            print("Item não pode ser vazio")
+            item = input("Insira o nome novamente: ")
+        elif item.lower() == "s":
+            print("Retornando para o menu")
+            break
+        else:
+            pilha.append(item)
+            break
+    
+empilhar()
