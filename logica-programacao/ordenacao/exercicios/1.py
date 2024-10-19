38ea4baeac5984be26c4724c61657f0f46a0d3b4
Felipe Gavião <batatasteve304@gmail.com>
2024-10-19 14:34:45 +0000

    c

--- logica-programacao/ordenacao/exercicios/1.py
+++ logica-programacao/ordenacao/exercicios/1.py
@@ -1,17 +1,47 @@
+import random, os
+
+os.system('cls || clear')
+
 lista = []
+
+def imprimir_em_ordem(lst):
+    for i, item in enumerate(lst, start=1):
+        print(f'{i}: {item}')
 
 while True:
     match input('''
-          1 - Cadastrar novo item
-          2 - Listar
-          3 - Listar Crescente
-          4 - Listar Decrescente
-          5 - Embaralhar
-          6 - Sair
+    1 - Cadastrar novo item
+    2 - Listar
+    3 - Listar Crescente
+    4 - Listar Decrescente
+    5 - Embaralhar
+    6 - Remover
+    7 - Sair
 '''):
         case '1':
             lista.append(input('Novo item:'))
         case '2':
-            print(lista, sep='\n')
+            imprimir_em_ordem(lista)
         case '3':
-            print(sorted(lista))
+            imprimir_em_ordem(sorted(lista))
+        case '4':
+            imprimir_em_ordem(sorted(lista, reverse=True))
+        case '5':
+            l = list(lista)
+            random.shuffle(l)
+            imprimir_em_ordem(l)
+        case '6':
+            numero = int(input())
+
+            if 0 > numero > len(lista):
+                print('Número especificado está fora da lista')
+                continue
+            
+            item = lista.pop(numero - 1)
+
+            print(f'Item {item} removido da lista')
+        case '7':
+            print('Saindo do programa...')
+            break
+        case _:
+            print('Comando inválido')
